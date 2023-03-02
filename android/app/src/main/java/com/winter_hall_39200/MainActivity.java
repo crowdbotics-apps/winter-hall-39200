553cb6c2b6b67b6455ed2d3936075793a32c3ff0
Crowdbotics <team@crowdbotics.com>
2023-03-02 21:56:43 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/winter_hall_39200/MainActivity.java
@@ -0,0 +1,15 @@
+package com.winter_hall_39200;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "winter_hall_39200";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "winter-hall-39200",
+  url: "https://winter-hall-39200.botics.co",
+  api: "https://winter-hall-39200.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
